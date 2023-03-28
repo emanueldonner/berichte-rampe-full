863406917e6da44b4d70bc0e90e73ded3491767b
emanueldonner <emanuel.donner@wh-d.at>
2023-03-28 12:30:42 +0200

    Added nunjucks as template engine.

--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const fastify = require("fastify")({
   logger: true,
 })
 
+fastify.register(require("@fastify/view"), {
+  engine: {
+    nunjucks: require("nunjucks"),
+  },
+})
+
 // Declare a route
 fastify.get("/", function (request, reply) {
-  // reply.send({ hello: "world" })
-  exec(
-    "./src/office-parser/parse.mjs -n ./public/output ./src/office-parser/samples/frameworkpraesentation_2021.docx",
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`)
-        return
-      }
-      console.log(`stdout: ${stdout}`)
-    }
-  )
+  reply.view("src/templates/index.njk", { text: "text" })
+
+  // exec(
+  //   "./src/office-parser/parse.mjs -n ./public/output ./src/office-parser/samples/frameworkpraesentation_2021.docx",
+  //   (error, stdout, stderr) => {
+  //     if (error) {
+  //       console.log(`error: ${error.message}`)
+  //       return
+  //     }
+  //     if (stderr) {
+  //       console.log(`stderr: ${stderr}`)
+  //       return
+  //     }
+  //     console.log(`stdout: ${stdout}`)
+  //   }
+  // )
 })
 
 // Run the server!
