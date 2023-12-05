caa18fed4ad95d6ba0c101564bdac6e0c7954b16
emanueldonner <emanuel.donner@wh-d.at>
2023-12-05 13:22:34 +0100

    Added helpful comments.

--- routes/upload.js
+++ routes/upload.js
@@ -3,47 +3,52 @@ const fs = require("fs-extra")
 const path = require("path")
 const sanitize = require("sanitize-filename")
 
+// Route für den Upload von Dokumenten
 async function uploadRoute(fastify, options) {
-  fastify.post("/upload", async function (request, reply) {
-    const { PUBLIC_DIR } = options
-    try {
-      fastify.connectionStore.broadcastMessage({
-        msg: "Dokument wird hochgeladen...",
-      })
-      const file = await request.file()
-      console.log("file", file.file)
-      if (!file) {
-        throw new Error("No file uploaded")
-      }
-      console.log("mime", file.mimetype)
-      if (
-        file.mimetype !==
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-      ) {
-        throw new Error("Invalid file type")
-      }
+	fastify.post("/upload", async function (request, reply) {
+		const { PUBLIC_DIR } = options
+		try {
+			// Startet Upload-Prozess und informiert Clients
+			fastify.connectionStore.broadcastMessage({
+				msg: "Dokument wird hochgeladen...",
+			})
+			const file = await request.file()
+			console.log("file", file.file)
+			if (!file) {
+				throw new Error("No file uploaded")
+			}
+			console.log("mime", file.mimetype)
+			// Überprüft, ob die Datei ein Word-Dokument ist
+			if (
+				file.mimetype !==
+				"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+			) {
+				throw new Error("Invalid file type")
+			}
 
-      const nameWithoutExt = sanitize(path.parse(file.filename).name)
-      const dir = `output/${nameWithoutExt}`
+			// Ermittelt den Dateinamen und erstellt den Upload-Ordner
+			const nameWithoutExt = sanitize(path.parse(file.filename).name)
+			const dir = `output/${nameWithoutExt}`
 
-      const uploadDir = path.join(PUBLIC_DIR, dir)
-      if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir, { recursive: true })
-      }
+			const uploadDir = path.join(PUBLIC_DIR, dir)
+			if (!fs.existsSync(uploadDir)) {
+				fs.mkdirSync(uploadDir, { recursive: true })
+			}
 
-      const savePath = path.join(uploadDir, file.filename)
-      await fs.promises.writeFile(savePath, file.file)
+			// Speichert die Datei im Upload-Ordner
+			const savePath = path.join(uploadDir, file.filename)
+			await fs.promises.writeFile(savePath, file.file)
 
-      reply.status(200).send({
-        message: "Dokument erfolgreich upgeloadet.",
-        status: "success",
-        path: uploadDir,
-        filename: file.filename,
-      })
-    } catch (error) {
-      reply.status(500).send({ message: error.message })
-    }
-  })
+			reply.status(200).send({
+				message: "Dokument erfolgreich upgeloadet.",
+				status: "success",
+				path: uploadDir,
+				filename: file.filename,
+			})
+		} catch (error) {
+			reply.status(500).send({ message: error.message })
+		}
+	})
 }
 
 module.exports = fp(uploadRoute)
