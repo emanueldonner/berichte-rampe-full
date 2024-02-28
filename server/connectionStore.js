9f40e03a4838c9f8f19b5469c816435d15c6d8ac
emanueldonner <emanuel.donner@wh-d.at>
2024-02-28 16:22:58 +0100

    Updated api doc

--- server/connectionStore.js
+++ server/connectionStore.js
@@ -1,31 +1,30 @@
 const WebSocket = require("ws")
 const fp = require("fastify-plugin")
 
-// connectionStore.js
 let connections = new Set()
 
 function addConnection(connection) {
-  connections.add(connection)
+	connections.add(connection)
 }
 
 function removeConnection(connection) {
-  connections.delete(connection)
+	connections.delete(connection)
 }
 
 function broadcastMessage(message) {
-  const messageString = JSON.stringify(message)
-  connections.forEach((connection) => {
-    if (connection.readyState === WebSocket.OPEN) {
-      connection.send(messageString)
-    }
-  })
+	const messageString = JSON.stringify(message)
+	connections.forEach((connection) => {
+		if (connection.readyState === WebSocket.OPEN) {
+			connection.send(messageString)
+		}
+	})
 }
 
 async function connectionStorePlugin(fastify, options) {
-  fastify.decorate("connectionStore", {
-    addConnection,
-    removeConnection,
-    broadcastMessage,
-  })
+	fastify.decorate("connectionStore", {
+		addConnection,
+		removeConnection,
+		broadcastMessage,
+	})
 }
 module.exports = fp(connectionStorePlugin)
